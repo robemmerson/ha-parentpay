f0ef56ce9eac7f8ae8c00f8932a13d5976f3f623
Rob Emmerson <rob.emmerson@gmail.com>
2026-04-19 13:16:02 +0100

    feat(const): add dismissals + backfill store keys, bump STORE_VERSION to 3

--- tests/test_store.py
+++ tests/test_store.py
@@ -62,3 +62,49 @@ async def test_merge_preserves_completed_flag_on_existing_purchases(
     await store.async_set_purchase_completed(purchase_uid, True)
     await store.async_merge([row])
     assert store.purchases[0]["completed"] is True
+
+
+async def test_store_v3_migration_wipes_v2_data(hass, hass_storage) -> None:
+    """When STORE_VERSION jumps from 2 to 3, _MigratingStore wipes prior caches."""
+    hass_storage["parentpay.meals_v1"] = {
+        "version": 2,
+        "minor_version": 1,
+        "key": "parentpay.meals_v1",
+        "data": [
+            {
+                "hash": "x",
+                "child_id": "11111111",
+                "date": "2026-01-01",
+                "item": "OLD ROW",
+                "amount_pence": 100,
+            }
+        ],
+    }
+    hass_storage["parentpay.purchases_v1"] = {
+        "version": 2,
+        "minor_version": 1,
+        "key": "parentpay.purchases_v1",
+        "data": [
+            {
+                "hash": "y",
+                "child_id": "11111111",
+                "date": "2026-01-01",
+                "item": "OLD PURCHASE",
+                "amount_pence": 200,
+                "completed": False,
+            }
+        ],
+    }
+    hass_storage["parentpay.payment_details_v1"] = {
+        "version": 2,
+        "minor_version": 1,
+        "key": "parentpay.payment_details_v1",
+        "data": {"old-tid": {"tid": "old-tid", "child_id": "11111111"}},
+    }
+
+    store = ParentPayStore(hass)  # picks up STORE_VERSION = 3
+    await store.async_load()
+
+    assert store.meals == []
+    assert store.purchases == []
+    assert store.get_payment_detail("old-tid") is None
